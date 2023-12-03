4e7c40b1b1833d04e183fb2e09452dfc80b2ca1d
Rue44 <muriithiruth44@gmail.com>
2023-12-03 22:50:25 +0300

    notenode

---
+++ middleware/errorHandler.js
@@ -0,0 +1,15 @@
+const { logEvents} = require ('./logger')
+
+const errorHander = (err, req, res, next ) => {
+logEvents (
+    `${err.name}: $ {err.message}\t${req.method}\t${req.url}\t$
+    {req.header.origin}`, 'errLog.log')
+    console.log(err.stack)
+
+
+const Status =  res.statusCode ? res.statusCode :500
+res.status (status);
+res.json({message: err.message})
+}
+
+module.exports = errorHander
